09df9cb5179162ce429c7daa0e2de5976e3ded66
tanialedemer <37988368+tanialedemer@users.noreply.github.com>
2018-10-12 08:32:41 +0200

    create command

---
+++ Command/Command/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Command.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Command
+{
+    public partial class App : Application
+    {
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new CommandPage();
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
